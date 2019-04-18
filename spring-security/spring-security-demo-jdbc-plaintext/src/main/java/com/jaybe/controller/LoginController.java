6ca9b34fb8469cf455269f12d5de79ad3f7d0061
Jabels <jabels1121@gmail.com>
2019-04-18 19:55:52 +0300

    Updated stored plain text passwords to bcrypted passwords

---
+++ spring-security/spring-security-demo-jdbc-plaintext/src/main/java/com/jaybe/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.jaybe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String getLoginForm() {
+        // return "plain-login";
+        return "fancy-login";
+    }
+
+    @GetMapping("/access-denied")
+    public String showUnauthorizedPage() {
+        // return "plain-login";
+        return "access-denied";
+    }
+
+}
---
+++ spring-security/spring-security-demo-jdbc-plaintext/src/main/webapp/WEB-INF/view/access-denied.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Jabels
+  Date: 4/10/2019
+  Time: 11:41 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Unauthorized</title>
+</head>
+<body>
+<h2>Access Denied - You are not authorized to access to that page</h2>
+<hr>
+<a href="${pageContext.request.contextPath}/">Back to home page</a>
+</body>
+</html>
